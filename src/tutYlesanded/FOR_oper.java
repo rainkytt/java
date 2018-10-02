5aa3b21c43329b7b9bbb775df6ac3b50685b801f
Rain <rain.kutt@khk.ee>
2018-10-02 21:01:33 +0300

    veel kodutöid

---
+++ src/tutYlesanded/FOR_oper.java
@@ -0,0 +1,9 @@
+package tutYlesanded;
+
+public class FOR_oper {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Loop " + i + " hello!");
+        }
+    }
+}
