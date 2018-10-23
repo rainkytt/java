186321078c64dc8f88e0ddc148ce36678d3e25a5
Rain <rain.kutt@khk.ee>
2018-10-23 12:46:05 +0300

    koodi kommenteeritud selgemaks

--- src/tutYlesanded/Car.java
+++ src/tutYlesanded/Car.java
@@ -1,19 +1,19 @@
 // Klassside näide, seotud klassiga Main
 package tutYlesanded;
 
-public class Car {
+public class Car { // "public" ehk piiramatu ligipääs ehk "access modifier"
 
-    private int doors;
+    private int doors; // kasutamiseks saadaval ainult "Car" klassile
     private int wheels;
     private String model;
     private String engine;
     private String color;
 
-    public void setModel(String model) {
-        this.model = model;
+    public void setModel(String model) { // loome avaliku meetodi (method to update a model), parameetriga "String"
+        this.model = model; // "this" is referring to a field in a class = parameetriks on "mudel"
     }
 
-    public String getModel() {
+    public String getModel() { // a method that can be call elsewhere to return what the current model is
         return this.model;
     }
 }
--- src/tutYlesanded/Main.java
+++ src/tutYlesanded/Main.java
@@ -1,12 +1,13 @@
-// Klasside näide seotud klassiga Car
+// Klasside näide video 25-26, seotud klassiga Car
 package tutYlesanded;
 
-public class Main {
+public class Main { // klassi (= objekt) tähistatakse suure algustähega "Main"
 
-    public static void main(String[] args) {
-        Car porsche = new Car();
-        Car holden = new Car();
-        porsche.setModel("Carrera");
+    public static void main(String[] args) { // meetodid algavad väikse algustähega "main"
+        Car porsche = new Car(); // created new datatype called "Porsche" and is of type "Car" (created an object called "Porsche" based on a template called "Car"), peale võrdust on selle "initialize it"
+        Car holden = new Car(); // peale võrdust = we used a constructor to create a car object
+        System.out.println("Model is " + porsche.getModel()); // andmata parameetriga
+        porsche.setModel("Carrera"); // calling a setModel method and updating it with parameter "Carrera"
         System.out.println("Model is " + porsche.getModel());
     }
 }
