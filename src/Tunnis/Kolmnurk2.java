a223122c9cfb603d0a1d9b72fcfe3405915844c5
Rain <rain.kutt@khk.ee>
2018-11-08 11:05:50 +0200

    ül 3: kolmnurk ühe tsükliga, uuem variant

--- src/Tunnis/Kolmnurk2.java
+++ src/Tunnis/Kolmnurk2.java
@@ -1,11 +1,16 @@
 // ül 3: kolmnurk ühe tsükliga
 package Tunnis;
+import java.util.Collections;
 
 public class Kolmnurk2 {
     public static void main(String[] args) {
         int rowNumbers = 5;
-        String symbols = "oooo*********";
-        for (int r = 0; r < rowNumbers; r++) {
+//        String symbols = "oooo*********";
+//        for (int r = 0; r < rowNumbers; r++) {
+//            System.out.println(symbols.substring(r, rowNumbers + 2 * r));
+        // variant 2
+        String symbols = String.join("", Collections.nCopies(rowNumbers-1, " ")) + String.join("", Collections.nCopies(rowNumbers*2-1, "*"));
+        for(int r = 0; r < rowNumbers; r++){
             System.out.println(symbols.substring(r, rowNumbers + 2 * r));
         }
     }
